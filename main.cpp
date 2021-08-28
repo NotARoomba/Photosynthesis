cb4638558e7b515af90cad2f601ae2f7c6ab7102
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2021-08-27 19:21:13 -0500

    external

--- main.cpp
+++ main.cpp
@@ -6,10 +6,6 @@ using namespace graphics;
 int main() {
 	Window window("Photosynthesis Engine", 800, 600);
 	while (!window.closed()) {
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glVertex2f(0.5f, -0.5f);
 		window.update();
 	}
 	return 0;
