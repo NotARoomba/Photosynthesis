6dbb9f1854504044e655f054500332150d2912fa
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2021-08-27 20:34:07 -0500

    triangle render

--- main.cpp
+++ main.cpp
@@ -5,9 +5,14 @@ using namespace graphics;
 
 int main() {
 	Window window("Photosynthesis Engine", 800, 600);
-	glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 	while (!window.closed()) {
 		window.clear();
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
 		window.update();
 	}
 	return 0;
--- src/window/window.h
+++ src/window/window.h
@@ -45,10 +45,6 @@ namespace photosynthesis {
 					return false;
 				}
 				glfwMakeContextCurrent(m_window);
-				glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-				glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-				glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-				glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 				return true;
 			}
 		};
