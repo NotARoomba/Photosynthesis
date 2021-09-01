e94a04758adb83a5e76412b1f1f296a2caa2bbd2
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2021-08-31 19:56:51 -0500

    mat4 and stuff

--- src/maths/maths.h
+++ src/maths/maths.h
@@ -1,5 +1,14 @@
 #pragma once
 
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include "vec2.h"
 #include "vec3.h"
 #include "vec4.h"
+#include "mat4.h"
+
+namespace photosynthesis {
+	namespace maths {
+		
+	}
+}
